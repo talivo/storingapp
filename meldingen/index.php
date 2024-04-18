7120a249fcf554acb196b9ed2201c82a21c172c6
talivo <76170849+talivo@users.noreply.github.com>
2024-04-18 16:01:49 +0200

    Eind-Hoofdstuk 7

--- meldingen/index.php
+++ meldingen/index.php
@@ -19,8 +19,46 @@
             echo "<div class='msg'>" . $_GET['msg'] . "</div>";
         } ?>
 
-        <div style="height: 300px; background: #ededed; display: flex; justify-content: center; align-items: center; color: #666666;">(hier komen de storingsmeldingen)</div>
+        <?php
+            require_once '../backend/conn.php';
+            $query = "SELECT * FROM meldingen";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+            $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC); 
+
+            ?>
+        <table>
+            <tr>
+                <th>Achtbaan</th>
+                <th>Type</th>
+                <th>Prioriteit</th>
+                <th>Capaciteit
+                <th>Gemeld op</th>
+                </th>
+                <th>Melder</th>
+                <th>Overige info</th>
+            </tr>
+            <?php foreach ($meldingen as $melding): ?>
+                <tr>
+                    <td><?php echo $melding['attractie']; ?></td>
+                    <td><?php echo $melding['type']; ?></td>
+                    <td><?php 
+                    
+                    if($melding['prioriteit'] == '1') {
+                        echo 'Ja';
+                    } else {
+                        echo 'Nee';
+                    }
+                    
+                    ?></td>
+                    <td><?php echo $melding['capaciteit']; ?></td>
+                    <td><?php echo $melding['melder']; ?></td>
+                    <td><?php echo $melding['overige_info']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
     </div>  
+    
 
 </body>
 
