413cd6632a09e63fc4609c722976b0db06e8dbf1
talivo <76170849+talivo@users.noreply.github.com>
2024-04-30 09:29:03 +0200

    register page setup

--- header.php
+++ header.php
@@ -7,9 +7,10 @@
             <a href="<?php echo $base_url; ?>/index.php">Home</a> |
             <a href="<?php echo $base_url; ?>/meldingen/index.php">Meldingen</a>
         </nav>
-        <div>
+        <div class="header-login-register">
             <?php if(!isset($_SESSION['user_id'])): ?>
                 <p><a href="<?php echo $base_url; ?>/login.php">Inloggen</a></p>
+                <p><a href="<?php echo $base_url; ?>/register.php">Registreer</a></p>
             <?php else: ?>
                 <p><a href="<?php echo $base_url; ?>/logout.php">Uitloggen</a></p>
             <?php endif; ?>
--- register.php
+++ register.php
@@ -10,14 +10,15 @@
 <!doctype html>
 <html lang="nl">
 <head>
-    <title>StoringApp / Login</title>
+    <title>StoringApp / Registreer</title>
     <?php require_once 'head.php'; ?>
 </head>
 <body>
     <?php require_once 'header.php'; ?>
     <div class="container home">
         <?php if(isset($_GET['msg'])){ echo $_GET['msg']; }?>
-        <form action="backend/loginController.php" method="POST">
+
+        <form action="backend/registerController.php" method="POST">
             <div class="form-group">
                 <label for="username">Gebruikersnaam: </label>
                 <input type="text" name="username" id="username">
@@ -26,8 +27,14 @@
                 <label for="password">Wachtwoord: </label>
                 <input type="password" name="password" id="password">
             </div>
-            <input type="submit" value="Login">
+            <div class="form-group">
+                <label for="password-confirm">Wachtwoord: </label>
+                <input type="password" name="password-confirm" id="password-confirm">
+            </div>
+            <input type="submit" value="Registreer">
         </form>
+
+
     </div>
 </body>
 </html>
